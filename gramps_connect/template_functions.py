3259716288f05a94ef2070172341bce62793dacd
Doug Blank <dblank@cs.brynmawr.edu>
2016-03-25 08:58:03 -0400

    9336: citation_ref as string on person table

--- gramps_connect/template_functions.py
+++ gramps_connect/template_functions.py
@@ -201,14 +201,14 @@ def name_table(form, user, action, link=None, **kwargs):
         count = 0
         for name in [form.instance.primary_name] + form.instance.alternate_names:
             citations = []
-            for citation_ref in name.citation_list:
-                citation = form.database.get_note_from_handle(citation_ref.ref)
+            for citation_handle in name.citation_list:
+                citation = form.database.get_note_from_handle(citation_handle)
                 if citation:
                     citations.append(citation)
             citationq = len(citations) > 0
             note_text = ""
-            for note_ref in name.note_list:
-                note = form.database.get_note_from_handle(note_ref.ref)
+            for note_handle in name.note_list:
+                note = form.database.get_note_from_handle(note_handle)
                 if note:
                     note_text = note.text.string[:50]
                     break
